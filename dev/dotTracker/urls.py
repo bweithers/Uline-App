f341c0c423db5285518f4ca47abafea924a13b13
bweithers <bweithers@Corellia.localdomain>
2019-04-24 11:21:04 -0500

    Added favico image

--- dev/dotTracker/urls.py
+++ dev/dotTracker/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.rider, name='rider'),
+    path('', views.driver, name='rider'),
     path('demo/', views.demo, name='demo'),
     path('driver/', views.driver, name='driver'),
 ]
